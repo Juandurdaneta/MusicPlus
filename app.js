59d9f12ade0db4531b116c7c4bc65c02cb9885f0
Juan Urdaneta <juandurdanet@gmail.com>
2021-06-22 16:35:06 -0400

    Añadiendo routing para paginas de registro y login

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get("/", (req,res) =>{
     res.render("index");
 })
 
+app.get("/login", (req, res) =>{
+    res.render("login");
+})
+
+app.get("/register", (req, res) =>{
+    res.render("register");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
