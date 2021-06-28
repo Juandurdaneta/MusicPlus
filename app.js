5914dc77a214052f8aa62a1958a42307f18a472d
Juan Urdaneta <juandurdanet@gmail.com>
2021-06-27 23:38:48 -0400

    Añadiendo hashing y perfil de usuario

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ const _ = require("lodash");
 const schema = require(__dirname + "/model/Schemas.js");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-app.use(session({ secret: "ssshhhhh", saveUninitialized: true, resave: true }));
+app.use(
+  session({
+    secret: "clave",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,6 +43,7 @@ const Album = schema.getAlbum();
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 // LOGIN
 
 app.get("/login", (req, res) => {
@@ -42,6 +51,8 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
+  const contraseñaIngresada = req.body.pass;
+
   Usuario.findOne(
     { email: req.body.correoElectronico },
     (err, usuarioEncontrado) => {
@@ -51,8 +62,11 @@ app.post("/login", (req, res) => {
           res.render("login", {
             MensajeError: "No hay cuenta para ese correo",
           });
-        } else if (usuarioEncontrado.password == req.body.pass) {
+        } else if (
+          bcrypt.compareSync(contraseñaIngresada, usuarioEncontrado.password)
+        ) {
           // SI LAS CONTRASEÑAS COINCIDEN ENTONCES SE INICIA LA SESION Y SE DESPACHA AL USUARIO A LA VISTA PRINCIPAL DE LA PAGINA
+          req.session.email = req.body.correoElectronico;
           res.redirect("/home");
         } else {
           res.render("login", {
@@ -74,25 +88,67 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+  const usuario = req.body.username;
+  const email = req.body.mail;
+  // HASHING
+  const pass = req.body.pass;
+  const hashPass = bcrypt.hashSync(pass, saltRounds);
+
   const nuevoUsuario = new Usuario({
     username: usuario,
     email: email,
-    password: contraseña,
+    password: hashPass,
   });
 
-  nuevoUsuario
-    .save()
-    .then(
-      res.render("register", {
-        UsuarioAgregado: "Te haz registrado exitosamente!",
-      })
-    );
+  nuevoUsuario.save().then(
+    res.render("register", {
+      UsuarioAgregado: "Te haz registrado exitosamente!",
+    })
+  );
 });
 
-app.get("/perfil/:id", (req, res) => {});
+// VISTA MAIN
 
 app.get("/home", (req, res) => {
-  res.render("homepage");
+  if (req.session.email == null) {
+    // SI LA SESION NO TIENE CORREO ELECTRONICO QUIERE DECIR QUE EL USUARIO NO HA INICIADO SESION, POR LO TANTO SE LE REDIRIGE AL LOGIN
+    res.redirect("/login");
+  } else {
+    Usuario.findOne({ email: req.session.email }, (err, usuarioEncontrado) => {
+      if (!err) {
+        res.render("homepage", { usuario: usuarioEncontrado._id });
+      }
+    });
+  }
+});
+
+// PERFIL DE USUARIO
+
+app.get("/perfil/:id", (req, res) => {
+
+  if (req.session.email == null) {
+    res.redirect("/login");
+  } else {
+    Usuario.findOne({ _id: req.params.id }, (err, UsuarioEncontrado) => {
+      if (!err) {
+        res.render("perfil", {
+          nombreUsuario: UsuarioEncontrado.username,
+          usuario: UsuarioEncontrado._id,
+          emailUsuario: UsuarioEncontrado.email,
+          emailSesion: req.session.email
+        });
+      } else {
+        console.log(err);
+      }
+    });
+  }
+});
+
+// CERRAR SESION
+
+app.get("/cerrar-sesion", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
--- model/Schemas.js
+++ model/Schemas.js
@@ -1,98 +1,83 @@
-const mongoose = require('mongoose'); 
+const mongoose = require("mongoose");
 // SCHEMA CANCION
 
 let cancionSchema = new mongoose.Schema({
-    nombreCancion: {
-        type: String,
-        required: true
-    },
-    FechaLanzamiento: {
-        type: Date
-    },
-    letras: {
-        type: String
-    }
-
+  nombreCancion: {
+    type: String,
+    required: true,
+  },
 });
-//SCHEMA ALBUM 
+//SCHEMA ALBUM
 
 const AlbumSchema = new mongoose.Schema({
-    nombreAlbum: {
-        type:String,
-        required:true
-    },
-    canciones : [cancionSchema],
-    portadaAlbum :{
-        type: String
-    }
-})
+  nombreAlbum: {
+    type: String,
+    required: true,
+  },
+  canciones: [cancionSchema],
+  portadaAlbum: {
+    type: String,
+  },
+});
 //SCHEMA PLAYLIST
 
 const playlistSchema = new mongoose.Schema({
-    nombre:{
-        type: String,
-        required: true
-    },
-    canciones:[cancionSchema]
+  nombre: {
+    type: String,
+    required: true,
+  },
+  canciones: [cancionSchema],
 });
 
 //SCHEMA ARTISTA
 
 const ArtistSchema = new mongoose.Schema({
-    nombreArtista:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    albumes:[AlbumSchema]
+  nombreArtista: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  albumes: [AlbumSchema],
 });
-
 
 // SCHEMA USUARIO
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    cancionesFavoritas : [cancionSchema],
-    playlists: [playlistSchema]
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cancionesFavoritas: [cancionSchema],
+  playlists: [playlistSchema],
 });
 
 //EXPORTANDO SCHEMAS
 
-exports.getArtista = function(){
-    return mongoose.model("Artista", ArtistSchema);
-}
-exports.getAlbum = function(){
-    return mongoose.model("Album", AlbumSchema);
-    
-}
-exports.getCancion = function(){
-    return mongoose.model("Cancion", cancionSchema);
+exports.getArtista = function () {
+  return mongoose.model("Artista", ArtistSchema);
+};
+exports.getAlbum = function () {
+  return mongoose.model("Album", AlbumSchema);
+};
+exports.getCancion = function () {
+  return mongoose.model("Cancion", cancionSchema);
+};
 
-}
+exports.getUsuario = function () {
+  return mongoose.model("Usuario", userSchema);
+};
 
-exports.getUsuario = function(){
-
-    return mongoose.model('Usuario', userSchema);
-}
-
-exports.getPlaylist = function(){
-
-    return mongoose.model('Playlist', playlistSchema);
-}
-
-
-
+exports.getPlaylist = function () {
+  return mongoose.model("Playlist", playlistSchema);
+};
